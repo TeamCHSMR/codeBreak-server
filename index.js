9d59d66b62500d0f5ff229d2b78ecacc9db03d1e
Tripp Lucas <tripppdx@gmail.com>
2021-12-20 10:55:35 -0800

    adds basic server

---
+++ index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const app = require('./lib/server');
+require('dotenv').config();
+
+const PORT = process.env.PORT || 3003;
+// const { db } = require('./lib/models');
+
+// db.sync().then(() => {
+//   app.start(PORT);
+// });
+app.start(PORT);
