4918fc6794470c2a96162abc37d8336cb3f04459
Tripp Lucas <tripppdx@gmail.com>
2021-12-20 11:17:47 -0800

    closes #1, closes #2, closes #3; adds user model, auth middlware and routes

--- index.js
+++ index.js
@@ -4,9 +4,8 @@ const app = require('./lib/server');
 require('dotenv').config();
 
 const PORT = process.env.PORT || 3003;
-// const { db } = require('./lib/models');
+const { db } = require('./lib/models');
 
-// db.sync().then(() => {
-//   app.start(PORT);
-// });
-app.start(PORT);
+db.sync().then(() => {
+  app.start(PORT);
+});
